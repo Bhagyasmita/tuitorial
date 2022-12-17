969f0dcdbf459d07b0c035fd0c4d43fb7a3d4710
Bhagyasmita Dwibedi <120777994+Bhagyasmita@users.noreply.github.com>
2022-12-17 16:02:42 +0530

    Add files via upload

---
+++ Student Result management/Project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author rohit
+ */
+public class ConnectionProvider {
+    public static Connection getcon()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Srm", "root","litu1234");
+            return con;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+    
+}
